70fab890e64f84432f6721293a7df5a4aa51e0d2
oiuv <i@oiuv.cn>
2018-05-27 23:50:22 +0800

    add github pages

--- config/index.js
+++ config/index.js
@@ -1,8 +1,8 @@
-'use strict'
+'use strict';
 // Template version: 1.3.1
 // see http://vuejs-templates.github.io/webpack for documentation.
 
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   dev: {
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/vuejs-essential/dist/',
 
     /**
      * Source Maps
