23660985aac80db1d54713a9efee87721e6fa908
oiuv <i@oiuv.cn>
2018-10-29 00:07:01 +0800

    build demo

--- config/index.js
+++ config/index.js
@@ -43,7 +43,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/vuejs-demo/dist/',
+    assetsPublicPath: '/vuejs-test/dist/',
 
     /**
      * Source Maps
