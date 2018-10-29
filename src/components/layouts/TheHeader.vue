039b24cd3c696e7ad9223e0f498ef56baf30358a
oiuv <i@oiuv.cn>
2018-10-29 09:01:49 +0800

    use bootstrap 4.0

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="navbar navbar-expand-md navbar-dark navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+                <a href="/" class="navbar-brand">
+                    <span class="title">{{ logo.title }}</span>
+                    <img :src="logo.src" :alt="logo.title">
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'TheHeader',
+        data() {
+            return {
+                logo: {
+                    src: `${this.url}/img/logo.gif`,
+                    title: 'VueJS'
+                }
+            }
+        },
+        beforeCreate() {
+            this.url = 'https://1000y.gameivy.com'
+        }
+    }
+</script>
+
+<style scoped>
+    .title { display: none;}
+</style>
