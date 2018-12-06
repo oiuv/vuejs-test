f4f9e762aaf15e96a89c412270b1777a845b7bf5
oiuv <i@oiuv.cn>
2018-12-06 23:40:19 +0800

    3.5:网站头尾

---
+++ src/components/layouts/TheFooter.vue
@@ -0,0 +1,33 @@
+<template>
+    <footer class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-7">
+                    由 <a :href="homepage" target="_blank">{{ author }}</a> 设计和编码 <span
+                        style="color: #e27575;font-size: 14px;">❤</span>
+                </div>
+
+                <div class="col-5 text-right"><a :href="email">联系我们</a></div>
+            </div>
+        </div>
+    </footer>
+
+</template>
+
+<script>
+
+    export default {
+        name: "TheFooter",
+        data() {
+            return {
+                author: '秋林拾叶',
+                homepage: 'https://github.com/oiuv/1000y.info',
+                email: 'mailto:i@gameivy.com'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/components/layouts/TheHeader.vue
+++ src/components/layouts/TheHeader.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="navbar navbar-expand-md navbar-dark navbar-static-top">
+    <nav class="navbar navbar-expand-md navbar-dark navbar-static-top fixed-top">
         <div class="container">
-            <div class="navbar-header">
-                <a href="/" class="navbar-brand">
-                    <span class="title">{{ logo.title }}</span>
-                    <img :src="logo.src" :alt="logo.title">
-                </a>
+            <a class="navbar-brand" href="/">
+                <span class="title">{{ logo.title }}</span>
+                <img :src="logo.src" :alt="logo.title" class="img-responsive">
+            </a>
+            <button class="navbar-toggler" type="button" @click="toggleNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div :class="['collapse', 'navbar-collapse', { show: showCollapsedNav }]" id="top-navbar-collapse">
+                <ul class="navbar-nav mr-auto">
+                    <li v-for="(item, index) in navList" class="nav-item" :class="{active : index === activeNavIndex}">
+                        <a href="#" @click="changeNavIndex(index)" class="nav-link">{{ item }}</a>
+                    </li>
+                </ul>
             </div>
         </div>
-    </div>
+    </nav>
 </template>
 
 <script>
@@ -18,16 +27,31 @@
             return {
                 logo: {
                     src: `${this.url}/img/logo.gif`,
-                    title: 'VueJS'
-                }
+                    title: '云端千年'
+                },
+                navList: ['首页', '话题', '分享', '问答', '反馈', '公告', '下载', '物品', '怪物', '玩家'],
+                activeNavIndex: 0,
+                showCollapsedNav: false
             }
         },
         beforeCreate() {
             this.url = 'https://1000y.gameivy.com'
+        },
+        methods: {
+            changeNavIndex(index) {
+                this.activeNavIndex = index
+            },
+            toggleNav() {
+                this.showCollapsedNav = !this.showCollapsedNav
+            }
         }
     }
 </script>
 
 <style scoped>
-    .title { display: none;}
+    .title {
+        display: none;
+    }
+
+
 </style>
